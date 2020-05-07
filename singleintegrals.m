1d6a9404593ad906ebfa0e2a56fcbdb9451d2f11
Jan Tuitman <jan_tuitman@hotmail.com>
2020-05-07 23:26:59 +0200

    fixed repeated index

--- singleintegrals.m
+++ singleintegrals.m
@@ -521,8 +521,8 @@ frobenius_pt:=function(P,data);
 
       C:=Coefficients(poly);
       D:=[];
-      for i:=1 to #C do
-        D[i]:=Evaluate(C[i],x0p); 
+      for j:=1 to #C do
+        D[j]:=Evaluate(C[j],x0p); 
       end for;
       fy:=Ky!D;
 
@@ -564,8 +564,8 @@ frobenius_pt:=function(P,data);
 
       C:=Coefficients(poly);
       D:=[];
-      for i:=1 to #C do
-        D[i]:=Evaluate(C[i],x0p); 
+      for j:=1 to #C do
+        D[j]:=Evaluate(C[j],x0p); 
       end for;
       fy:=Ky!D;
 
@@ -1346,8 +1346,8 @@ find_bad_point_in_disk:=function(P,data);
       poly:=minpoly(xfun,bfun[i]);
       C:=Coefficients(poly);
       D:=[];
-      for i:=1 to #C do
-        D[i]:=Evaluate(C[i],x0); 
+      for j:=1 to #C do
+        D[j]:=Evaluate(C[j],x0); 
       end for;
       fy:=Ky!D;
       fac:=Factorisation(fy);
@@ -1390,8 +1390,8 @@ find_bad_point_in_disk:=function(P,data);
         poly:=minpoly(bindex,bfun[i]);
         C:=Coefficients(poly);
         D:=[];
-        for i:=1 to #C do
-          D[i]:=Evaluate(C[i],b[index]); 
+        for j:=1 to #C do
+          D[j]:=Evaluate(C[j],b[index]); 
         end for;
         fy:=Ky!D;
         fac:=Factorisation(fy); // Roots has some problems that Factorisation does not
